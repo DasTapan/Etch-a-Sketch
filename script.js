edb48a411447192badfc1fd1ceeb73182ad967e3
Tapan Das <tapands.das@gmail.com>
2022-04-17 13:09:05 +0530

    Add hover effect to grids

--- script.js
+++ script.js
@@ -7,17 +7,27 @@ for (let i = 0; i < 256; i++) {
     divId++;
     div.style.width = 'calc(100% / 16)';
     div.style.height = 'calc(100% / 16)';
-    // console.log(div.style.width);
-    // console.log(div.style.height);
     containerDiv.appendChild(div);
-    // console.log(div);
 }
-// listen & take user input from RESET
+
+//listen for mouse cursor on container div
+//(this trigers hover function to change background)
+containerDiv.addEventListener('mouseenter', hover);
+
+//define hover logic on divs
+function hover() {
+    const divs = document.querySelectorAll('div');
+    divs.forEach((div) => {
+        div.addEventListener('mouseenter', () => {
+            div.classList.add('customBg');
+        })
+    })
+}
+
+//take user input through RESET button
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
-    // console.log(button);
     let userInput = Number(window.prompt('Enter the number of boxes(n * n)', ''));
-    console.log(userInput);
     //pass the value to create the custom box
     if (userInput > 100) {
         while (userInput > 100) {
@@ -30,6 +40,7 @@ button.addEventListener('click', () => {
 function generateGrid(num) {
     //clean the box
     containerDiv.replaceChildren();
+    //create new layout
     let divId = 1;
     for (let i = 0; i < (num * num); i++) {
         const div = document.createElement('div');
@@ -37,10 +48,6 @@ function generateGrid(num) {
         divId++;
         div.style.width = `calc(100% / ${num})`;
         div.style.height = `calc(100% / ${num})`;
-        // console.log(div.style.width);
-        // console.log(div.style.height);
         containerDiv.appendChild(div);
-        // console.log(div);
     }
-
 }
